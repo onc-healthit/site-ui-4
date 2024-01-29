75bbd806c975a712d45606f2c4d7ccc47f2ef1b9
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-01-29 14:26:31 -0500

    ui: change nav width to help with spacing in header

--- src/constants/navConstants.ts
+++ src/constants/navConstants.ts
@@ -1,4 +1,4 @@
-export const DRAWER_WIDTH: number = 300
+export const DRAWER_WIDTH: number = 316
 export const NAV_PADDING_LEFT: number = 2
 export const GENERIC_LINK_TO_REPLACE: string = 'https://www.healthit.gov'
 export const SITE_VERSION: string = '4.0-static'
