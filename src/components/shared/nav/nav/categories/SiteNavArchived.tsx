b4ed0dba45347867e49b71da55634008bccd9fbc
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-01-29 13:13:49 -0500

    ui: add divider and ListItemButton to Archive section

--- src/components/shared/nav/nav/categories/SiteNavArchived.tsx
+++ src/components/shared/nav/nav/categories/SiteNavArchived.tsx
@@ -1,4 +1,4 @@
-import { ListItemButton, ListItemIcon, ListItemText } from '@mui/material/'
+import { Divider, ListItemButton, ListItemIcon, ListItemText } from '@mui/material/'
 import Link from 'next/link'
 
 import { ArchiveOutlined } from '@mui/icons-material'
@@ -6,17 +6,20 @@ import palette from '@/styles/palette'
 
 export default function SiteNavArchived() {
   return (
-    <Link href="/archived" passHref>
-      <ListItemButton>
-        <ListItemIcon>
-          <ArchiveOutlined color="primary" />
-          <ListItemText
-            primaryTypographyProps={{ color: palette.primary }}
-            primary="Archived"
-            sx={{ pl: 4, color: 'inherit' }}
-          />
-        </ListItemIcon>
-      </ListItemButton>
-    </Link>
+    <>
+      <Link href="/archived" passHref>
+        <ListItemButton>
+          <ListItemIcon>
+            <ArchiveOutlined color="primary" />
+            <ListItemText
+              primaryTypographyProps={{ color: palette.primary }}
+              primary="Archived"
+              sx={{ pl: 4, color: 'inherit' }}
+            />
+          </ListItemIcon>
+        </ListItemButton>
+      </Link>
+      <Divider />
+    </>
   )
 }
