7b459f100c6c1a258660a82dbdb6a9bd642ae71a
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-02-05 12:56:04 -0500

    ui: add box shadow to ankle

--- src/components/shared/Ankle.tsx
+++ src/components/shared/Ankle.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Ankle() {
   return (
-    <Box bgcolor="#122953" pt={4} pb={4}>
+    <Box bgcolor="#122953" boxShadow={'0px -4px 20px 20px rgb(0, 0, 0, 0.15)'} pt={4} pb={4}>
       <Container>
         <Typography variant="h5" color="#fff" component={'h2'}>
           <strong>Cant find what your looking for?</strong>
