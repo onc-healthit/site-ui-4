ae194aa81d875cd45b03e152f460fccf14122f37
ebrockainq <ebrock@ainq.com>
2024-03-11 13:22:21 -0400

    Improved grid scaling, formatting improvements

--- src/components/direct/hisp/ValidationResultsTab.tsx
+++ src/components/direct/hisp/ValidationResultsTab.tsx
@@ -1,51 +1,42 @@
 import * as React from 'react'
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Grid, Typography, Container } from '@mui/material'
 import ProfilesCard from './ProfilesCard'
 
 const ValidationResults = () => {
   return (
-    <Box
-      sx={{
-        padding: '32px',
-        width: '100%',
-        minHeight: 'auto',
-        pl: '200px',
-      }}
-    >
-      <Typography variant="h4" sx={{ mb: 4 }}>
-        Below are the different profiles you are aligned to. Select one to see the validation results.
-      </Typography>
+    <Container>
+      <Box
+        sx={{
+          padding: '32px',
+          width: '1391px',
+          minHeight: 'auto',
+        }}
+      >
+        <Typography variant="h4" sx={{ mb: 4 }}>
+          Below are the different profiles you are aligned to. Select one to see the validation results.
+        </Typography>
 
-      <Grid container spacing={4} sx={{ justifyContent: 'flex-start' }}>
-        <Grid item sx={{ minWidth: 419 }}>
+        <Box gap={4} display={'flex'} flexDirection={'row'} flexWrap={'wrap'}>
           <ProfilesCard header=".gov" smtpAddress="direct.ett.health.gov" emailAddress="direct.ett.health.gov" />
-        </Grid>
-        <Grid item sx={{ minWidth: 419 }}>
           <ProfilesCard
             header="HIT2.nist.gov"
             smtpAddress="direct.ett.health.gov"
             emailAddress="direct.ett.health.gov"
           />
-        </Grid>
-        <Grid item sx={{ minWidth: 419 }}>
           <ProfilesCard
             header=".Default Profile 3"
             smtpAddress="direct.ett.health.gov"
             emailAddress="direct.ett.health.gov"
           />
-        </Grid>
-        <Grid item sx={{ minWidth: 419 }}>
           <ProfilesCard header="test.gov" smtpAddress="direct.ett.health.gov" emailAddress="direct.ett.health.gov" />
-        </Grid>
-        <Grid item sx={{ minWidth: 419 }}>
           <ProfilesCard
             header="HIT1.nist.gov"
             smtpAddress="direct.ett.health.gov"
             emailAddress="direct.ett.health.gov"
           />
-        </Grid>
-      </Grid>
-    </Box>
+        </Box>
+      </Box>
+    </Container>
   )
 }
 
