66d6cf5ec9fa7486d2d535d2819f87010fddb7e8
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-03-13 15:14:19 -0400

    ui: add additional textfileds and make dropdown control test being shown

--- src/components/direct/hisp/XDRTab.tsx
+++ src/components/direct/hisp/XDRTab.tsx
@@ -1,5 +1,8 @@
+import React, { useState } from 'react'
+
 import {
   Box,
+  Button,
   Container,
   Card,
   CardContent,
@@ -9,22 +12,18 @@ import {
   Select,
   MenuItem,
   SelectChangeEvent,
+  Divider,
 } from '@mui/material'
-import React from 'react'
-import { useEffect } from 'react'
 import XDRTestCard from '@components/direct/hisp/XDRTestCard'
+import { Download } from '@mui/icons-material'
+import palette from '@/styles/palette'
 const XDR = () => {
-  const [option, setOption] = React.useState('')
-  const [showTestCard, setShowTestCard] = React.useState(false)
+  const [option, setOption] = useState('')
+
   const handleChange = (event: SelectChangeEvent) => {
     setOption(event.target.value as string)
   }
 
-  useEffect(() => {
-    if (option !== '') {
-      setShowTestCard(true)
-    }
-  }, [option])
   const dropdown = [
     {
       value: 'Sender',
@@ -43,8 +42,8 @@ const XDR = () => {
         </Typography>
       </Box>
       <Box display={'flex'} flexDirection={'row'} gap={4} py={4}>
-        <Box width={'40%'}>
-          <Card>
+        <Box sx={{ position: 'sticky', top: 0, zIndex: 100 }} display={'flex'} gap={4} flexDirection={'column'}>
+          <Card elevation={4}>
             <CardContent>
               <FormControl fullWidth>
                 <InputLabel id="select-label">Choose a sender or reciever to see the test </InputLabel>
@@ -64,30 +63,211 @@ const XDR = () => {
               </FormControl>
             </CardContent>
           </Card>
+          <Card elevation={4}>
+            <CardContent>
+              <Box display={'flex'} flexDirection={'column'} alignItems={'flex-start'} gap={2} pt={1}>
+                <Typography>You can also download XDR TLS certificates.</Typography>
+                <Button variant="outlined" color="primary" endIcon={<Download />}>
+                  Download
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
         </Box>
         <Box gap={4} display={'flex'} flexDirection={'column'} width={'60%'}>
-          {showTestCard && (
+          {option === 'Sender' && (
             <XDRTestCard
-              cardHeader={'SMTP MT Test 39 (Message with Good Header)'}
+              cardHeader={'XDR Test 10'}
               cardContent={
-                'Verify the ability of the system to properly process the Disposition-Notifications-Options Header. Hitting Run will cause ETT to send a message with a well-formed Disposition-Notifications-Options Header. If the message is sent successfully. Check MDN will appear. Clicking that will check for the expected Processed and Dispatched MDN (Note that you may have to click multiple times due to lag.) The expected result is a Processed and Dispatched MDN. The selection of a file is optional, if the system requires clinical information with the message.'
+                'Test Tool sends a Direct Message to the SUT. The SUT must translate this to an XDR message and send it back to the Edge. Verify that the HISP system can create an XDR message per the specification and forward to Edge. The return endpoint is provided below. The validation report will be sent to the email address registered with the Direct address enter during setup. Provide your Direct To Address. Hit Run to generate your endpoint'
               }
+              links={[
+                { label: 'Endpoint', href: 'http://ett.healthit.gov:11080/xdstools/sim/edge-ttp__10/rep/xdrpr' },
+                { label: 'Endpoint TLS', href: 'https://ett.healthit.gov:11084/xdstools/sim/edge-ttp__10/rep/xdrpr' },
+              ]}
+              cardInput="Direct To Address"
+              helperText="Direct To Address: SUTs receiving email endpoint for Direct/XDR translation, ETT to SUT workflow."
+              directFromInput={null}
+              endpointInput={null}
+              timeoutInput={null}
+              outgoingInput={null}
             />
           )}
-          {showTestCard && (
+          {option === 'Sender' && (
             <XDRTestCard
-              cardHeader={'SMTP MT Test 40 (Message with Bad Header)'}
+              cardHeader={'XDR Test 11'}
               cardContent={
-                'Verify the ability of the system to process an invalid Disposition-Notifications-Options Header. Hitting Run will cause ETT to send a message with an incorrect Disposition-Notifications-Options Header. If the message is sent successfully. Check MDN will appear. Clicking that will check for the expected Processed MDN (Note that you may have to click multiple times due to lag.) The expected result is that the Vendor will be able to process the invalid Disposition-Notifications-Options Header and respond with a Processed MDN. Optionally, the test will accommodate systems that also respond with a Dispatched MDN--however the Dispatched MDN must NOT contain the X-DIRECT-FINAL-DESTINATION-DELIVERY header to pass. The selection of a file is optional, if the system requires clinical information with the message.'
+                'Test Tool sends a Direct Message + XDM to the SUT. The SUT must translate this to an XDR message with Limited Metadata and send it back to the Edge. Verify that the HISP system can create an XDR message per the specification and forward to Edge. The return endpoint is provided below. The validation report will be sent to the email address registered with the Direct address enter during setup. Provide your Direct To Address. Hit Run to generate your endpoint.'
               }
+              links={[
+                { label: 'Endpoint', href: 'http://ett.healthit.gov:11080/xdstools/sim/edge-ttp__11/rep/xdrpr' },
+                { label: 'Endpoint TLS', href: 'https://ett.healthit.gov:11084/xdstools/sim/edge-ttp__11/rep/xdrpr' },
+              ]}
+              cardInput="Direct To Address"
+              helperText="Direct To Address: SUTs receiving email endpoint for Direct/XDR translation, ETT to SUT workflow."
+              directFromInput={null}
+              endpointInput={null}
+              timeoutInput={null}
+              outgoingInput={null}
             />
           )}
-          {showTestCard && (
+          {option === 'Sender' && (
             <XDRTestCard
-              cardHeader={'SMTP MT Test 41 (Unable to deliver)'}
+              cardHeader={'XDR Test 12'}
               cardContent={
-                'Verifies the ability of the sending system to send an email to ETT using the SMTP protocol with STARTTLS. The SUT will send an email to edge-receiver@james.healthit.gov. Hitting "Run" will cause ETT to search for an email sent to edge-receiver@james.healthit.gov from the email address entered in Profile window. Note that the C-CDA Document Type selected will not affect the test result.'
+                'Test Tool sends a Direct Message + XDM to the SUT. The SUT must translate this to an XDR message with Full Metadata and send it back to the Edge. Verify that the HISP system can create an XDR message per the specification and forward to Edge. The return endpoint is provided below.The validation report will be sent to the email address registered with the Direct address enter during setup. Provide your Direct To Address. Hit Run to generate your endpoint.'
               }
+              links={[
+                { label: 'Endpoint', href: 'http://ett.healthit.gov:11080/xdstools/sim/edge-ttp__12/rep/xdrpr' },
+                { label: 'Endpoint TLS', href: 'https://ett.healthit.gov:11084/xdstools/sim/edge-ttp__12/rep/xdrpr' },
+              ]}
+              cardInput="Direct To Address"
+              helperText="Direct To Address: SUTs receiving email endpoint for Direct/XDR translation, ETT to SUT workflow."
+              directFromInput={null}
+              endpointInput={null}
+              timeoutInput={null}
+              outgoingInput={null}
+            />
+          )}
+          {option === 'Sender' && (
+            <XDRTestCard
+              cardHeader={'XDR Test 12'}
+              cardContent={
+                'Test Tool sends a Direct Message + XDM to the SUT. The SUT must translate this to an XDR message with Full Metadata and send it back to the Edge. Verify that the HISP system can create an XDR message per the specification and forward to Edge. The return endpoint is provided below.The validation report will be sent to the email address registered with the Direct address enter during setup. Provide your Direct To Address. Hit Run to generate your endpoint.'
+              }
+              links={[
+                { label: 'Endpoint', href: 'http://ett.healthit.gov:11080/xdstools/sim/edge-ttp__12/rep/xdrpr' },
+                { label: 'Endpoint TLS', href: 'https://ett.healthit.gov:11084/xdstools/sim/edge-ttp__12/rep/xdrpr' },
+              ]}
+              cardInput="Direct To Address"
+              helperText="Direct To Address: SUT's receiving email endpoint for Direct/XDR translation, ETT to SUT workflow."
+              directFromInput={null}
+              endpointInput={null}
+              timeoutInput={null}
+              outgoingInput={null}
+            />
+          )}
+          {option === 'Sender' && (
+            <XDRTestCard
+              cardHeader={'XDR Test 16'}
+              cardContent={
+                'Verify that Mutual TLS session is established between the Sender and the Receiver before transimitting data. Provide your Direct From Address. Hit Run to generate your endpoint.'
+              }
+              links={null}
+              cardInput="Direct From Address"
+              helperText="Direct From Address: SUT (HISP) outgoing SMTP address (or) MDN."
+              directFromInput={null}
+              endpointInput={null}
+              timeoutInput={null}
+              outgoingInput={null}
+            />
+          )}
+          {option === 'Sender' && (
+            <XDRTestCard
+              cardHeader={'XDR Test 17'}
+              cardContent={
+                'Verify that HISP disconnects when the Server provided certificate is invalid. Only the IP address of the SUT shall be entered below. As this is a socket based test, the full endpoint is not required. Only the IP Address and port are needed and provided. Provide your IP Address. Hit Run to generate your endpoint'
+              }
+              links={null}
+              cardInput="IP Address:"
+              helperText="IP Address (eg: 202.255.24.62)"
+              directFromInput={null}
+              endpointInput={null}
+              timeoutInput={null}
+              outgoingInput={null}
+            />
+          )}
+
+          {/* Reciever Test */}
+
+          {option === 'Reciever' && (
+            <XDRTestCard
+              cardHeader={'XDR Test 17'}
+              cardContent={
+                'Verify that HISP disconnects when the Server provided certificate is invalid. Only the IP address of the SUT shall be entered below. As this is a socket based test, the full endpoint is not required. Only the IP Address and port are needed and provided. Provide your IP Address. Hit Run to generate your endpoint'
+              }
+              links={null}
+              cardInput="IP Address:"
+              helperText="IP Address (eg: 202.255.24.62)"
+              directFromInput={null}
+              endpointInput={null}
+              timeoutInput={null}
+              outgoingInput={null}
+            />
+          )}
+          {option === 'Reciever' && (
+            <XDRTestCard
+              cardHeader={'XDR Test 17'}
+              cardContent={
+                'Verify that HISP disconnects when the Server provided certificate is invalid. Only the IP address of the SUT shall be entered below. As this is a socket based test, the full endpoint is not required. Only the IP Address and port are needed and provided. Provide your IP Address. Hit Run to generate your endpoint'
+              }
+              links={null}
+              cardInput="IP Address:"
+              helperText="IP Address (eg: 202.255.24.62)"
+              directFromInput={null}
+              endpointInput={null}
+              timeoutInput={null}
+              outgoingInput={null}
+            />
+          )}
+          {option === 'Reciever' && (
+            <XDRTestCard
+              cardHeader={'XDR Test 17'}
+              cardContent={
+                'Verify that HISP disconnects when the Server provided certificate is invalid. Only the IP address of the SUT shall be entered below. As this is a socket based test, the full endpoint is not required. Only the IP Address and port are needed and provided. Provide your IP Address. Hit Run to generate your endpoint'
+              }
+              links={null}
+              cardInput="IP Address:"
+              helperText="IP Address (eg: 202.255.24.62)"
+              directFromInput={null}
+              endpointInput={null}
+              timeoutInput={null}
+              outgoingInput={null}
+            />
+          )}
+          {option === 'Reciever' && (
+            <XDRTestCard
+              cardHeader={'XDR Test 17'}
+              cardContent={
+                'Verify that HISP disconnects when the Server provided certificate is invalid. Only the IP address of the SUT shall be entered below. As this is a socket based test, the full endpoint is not required. Only the IP Address and port are needed and provided. Provide your IP Address. Hit Run to generate your endpoint'
+              }
+              links={null}
+              cardInput="IP Address:"
+              helperText="IP Address (eg: 202.255.24.62)"
+              directFromInput={null}
+              endpointInput={null}
+              timeoutInput={null}
+              outgoingInput={null}
+            />
+          )}
+          {option === 'Reciever' && (
+            <XDRTestCard
+              cardHeader={'XDR Test 17'}
+              cardContent={
+                'Verify that HISP disconnects when the Server provided certificate is invalid. Only the IP address of the SUT shall be entered below. As this is a socket based test, the full endpoint is not required. Only the IP Address and port are needed and provided. Provide your IP Address. Hit Run to generate your endpoint'
+              }
+              links={null}
+              cardInput="IP Address:"
+              helperText="IP Address (eg: 202.255.24.62)"
+              directFromInput={null}
+              endpointInput={null}
+              timeoutInput={null}
+              outgoingInput={null}
+            />
+          )}
+          {option === 'Reciever' && (
+            <XDRTestCard
+              cardHeader={'XDR Test 17'}
+              cardContent={
+                'Verify that HISP disconnects when the Server provided certificate is invalid. Only the IP address of the SUT shall be entered below. As this is a socket based test, the full endpoint is not required. Only the IP Address and port are needed and provided. Provide your IP Address. Hit Run to generate your endpoint'
+              }
+              links={null}
+              cardInput="IP Address:"
+              helperText="IP Address (eg: 202.255.24.62)"
+              directFromInput={null}
+              endpointInput={null}
+              timeoutInput={null}
+              outgoingInput={null}
             />
           )}
         </Box>
--- src/components/direct/hisp/XDRTestCard.tsx
+++ src/components/direct/hisp/XDRTestCard.tsx
@@ -1,38 +1,150 @@
-import { Box, Button, Card, CardContent, CardHeader, Divider, Typography } from '@mui/material'
-import InfoIcon from '@mui/icons-material/Info'
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  TextField,
+  Typography,
+  FormControl,
+  FormHelperText,
+} from '@mui/material'
+
+import ContentPasteGoIcon from '@mui/icons-material/ContentPasteGo'
+
 export interface TestCardProps {
   cardHeader: string
   cardContent: string
+  cardInput: string
+  links?: { label: string; href: string }[] | null
+  helperText: string
+  directFromInput: string | null
+  endpointInput: string | null
+  timeoutInput: string | null
+  outgoingInput: string | null
+}
+const handleClick = (link: string) => {
+  navigator.clipboard
+    .writeText(link)
+    .then(() => {
+      alert('Link copied to clipboard: ' + link)
+    })
+    .catch((err) => {
+      console.error('Failed to copy link: ', err)
+      alert('Failed to copy link. Please try again.')
+    })
 }
 
-const TestCard = ({ cardHeader, cardContent }: TestCardProps) => {
+const TestCard = ({
+  cardHeader,
+  cardContent,
+  directFromInput,
+  endpointInput,
+  timeoutInput,
+  outgoingInput,
+  links,
+  helperText,
+}: TestCardProps) => {
   return (
     <Card>
       <CardHeader title={cardHeader}></CardHeader>
       <Divider />
       <CardContent>
         <Typography variant="body2">{cardContent}</Typography>
+        {directFromInput !== null && (
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '100%',
+              pt: 2,
+            }}
+          >
+            <FormControl fullWidth>
+              <TextField fullWidth id="card-input" label={directFromInput} variant="outlined" />
+              <FormHelperText>{helperText}</FormHelperText>
+            </FormControl>
+          </Box>
+        )}
+        {endpointInput !== null && (
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '100%',
+              pt: 2,
+            }}
+          >
+            <FormControl fullWidth>
+              <TextField fullWidth id="card-input" label={endpointInput} variant="outlined" />
+              <FormHelperText>{helperText}</FormHelperText>
+            </FormControl>
+          </Box>
+        )}
+        {timeoutInput !== null && (
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '100%',
+              pt: 2,
+            }}
+          >
+            <FormControl fullWidth>
+              <TextField fullWidth id="card-input" label={timeoutInput} variant="outlined" />
+              <FormHelperText>{helperText}</FormHelperText>
+            </FormControl>
+          </Box>
+        )}
+        {outgoingInput !== null && (
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '100%',
+              pt: 2,
+            }}
+          >
+            <FormControl fullWidth>
+              <TextField fullWidth id="card-input" label={outgoingInput} variant="outlined" />
+              <FormHelperText>{helperText}</FormHelperText>
+            </FormControl>
+          </Box>
+        )}
       </CardContent>
       <Divider />
+
       <Box
         sx={{
           display: 'flex',
           flexDirection: 'row',
+          alignItems: 'center',
           justifyContent: 'space-between',
-          alignItems: 'end',
-          width: '100%',
-          p: 2,
+          gap: 1,
+          paddingY: 2,
+          pr: 2,
         }}
       >
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-          <Typography>
-            CCDA Document Type <InfoIcon color="primary" fontSize="small" />
-          </Typography>
-          <Button variant="outlined" color="primary">
-            SELECT A DOCUMENT
-          </Button>
+        <Box width={'50%'}>
+          {links && links.length > 0 && (
+            <Box>
+              {links.map((link, index) => (
+                <Button
+                  sx={{ ml: 2 }}
+                  key={index}
+                  color="secondary"
+                  endIcon={<ContentPasteGoIcon color="secondary" />}
+                  onClick={() => handleClick(link.href)}
+                >
+                  {link.label}
+                </Button>
+              ))}
+            </Box>
+          )}
         </Box>
-        <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1 }}>
+
+        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', gap: 1 }}>
           <Button variant="contained" color="primary" disabled>
             RUN
           </Button>
