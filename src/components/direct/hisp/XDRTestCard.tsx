fe3fd62fa9b4867ab3e600a9c3d5d67402aca1f0
akanuri <akanuri@ainq.com>
2024-03-21 17:42:44 -0400

    Enhancements to HISP XDR page

--- src/components/direct/hisp/XDRTestCard.tsx
+++ src/components/direct/hisp/XDRTestCard.tsx
@@ -12,22 +12,46 @@ import {
 } from '@mui/material'
 
 import ContentPasteGoIcon from '@mui/icons-material/ContentPasteGo'
+import _ from 'lodash'
+
+export type TestCaseFields = {
+  name?: string
+  id?: string | number
+  ID?: string
+  desc?: string
+  sutEdge?: boolean
+  sutHisp?: boolean
+  sutRole?: string
+  criteria?: string
+  status?: string
+  inputs?: InputFields[]
+  'Test Focus'?: string
+  'Data Flow in Direct'?: string
+  'Metadata Included'?: string
+  'Direct Address Block'?: string
+  'SUT: Sender/ Receiver'?: string
+  'Test Tool: Sender / Receiver'?: string
+  'Purpose/Description'?: string
+  'Conformance  Test Details'?: string
+  'Expected Test Results'?: string
+  'Required / Conditional per Direct Edge Protocol Guide'?: string
+  'Direct Edge Protocol Guide 1.1 RTM Reference'?: number
+  'Test Data IDs'?: string | null
+}
+
+export type InputFields = {
+  name?: string
+  hoverlabel?: string
+  key?: string
+  type?: string
+}
 
 export interface TestCardProps {
-  cardHeader: string
-  cardContent: string
-  cardInput: string | null
-  links?: { label: string; href: string }[] | null
-  directFromInput: string | null
-  endpointInput: string | null
-  timeoutInput: string | null
-  outgoingInput: string | null
-  portInput: string | null
-  ipInput: string | null
-  helperText: string | null
-  helperOutgoingText: string | null
-  helperTimeoutText: string | null
-  helperEndpointText: string | null
+  test: TestCaseFields
+}
+interface StepTextProps {
+  inputs: InputFields[]
+  role?: string
 }
 const handleClick = (link: string) => {
   navigator.clipboard
@@ -47,78 +71,61 @@ const flexColumnStyle = {
   pt: 2,
 }
 
-const TestCard = ({
-  cardHeader,
-  cardContent,
-  directFromInput,
-  endpointInput,
-  timeoutInput,
-  outgoingInput,
-  links,
-  helperText,
-  helperEndpointText,
-  helperOutgoingText,
-  helperTimeoutText,
-  portInput,
-  ipInput,
-}: TestCardProps) => {
+const senderText = 'Hit Run to generate your endpoint.'
+const receiverText = 'Hit Run to send a XDR message.'
+const endpoint = 'http://ett.healthit.gov:11080/xdstools/sim/'
+const endpointTLS = 'https://ett.healthit.gov:11084/xdstools/sim/'
+
+const StepText = ({ inputs, role }: StepTextProps) => {
+  return (
+    <>
+      <Typography variant="body2">
+        <strong>Step 1</strong>: Provide your{' '}
+        {inputs.map((input, i) => {
+          return (
+            <span key={i}>
+              {input.name}
+              {inputs !== undefined ? (i === inputs.length - 1 ? '. ' : ', ') : '. '}
+            </span>
+          )
+        })}
+        {role === 'sender' && senderText}
+        {role === 'receiver' && receiverText}
+      </Typography>
+    </>
+  )
+}
+const TestCard = ({ test }: TestCardProps) => {
   return (
     <Card>
-      <CardHeader title={cardHeader}></CardHeader>
+      <CardHeader title={test.name}></CardHeader>
       <Divider />
       <CardContent>
-        <Typography variant="body2">{cardContent}</Typography>
-        {directFromInput !== null && (
-          <Box sx={flexColumnStyle}>
-            <FormControl fullWidth>
-              <TextField fullWidth id="card-input" label={directFromInput} variant="outlined" />
-              <FormHelperText>{helperText}</FormHelperText>
-            </FormControl>
-          </Box>
+        <Typography variant="body2" sx={{ pb: 2 }}>
+          {test.desc}
+        </Typography>
+        {_.isEqual(test.sutRole, 'receiver') && _.has(test, 'inputs') && test.inputs !== undefined && (
+          <StepText inputs={test.inputs} role={test.sutRole} />
         )}
-        {endpointInput !== null && (
-          <Box sx={flexColumnStyle}>
-            <FormControl fullWidth>
-              <TextField fullWidth id="card-input" label={endpointInput} variant="outlined" />
-              <FormHelperText>{helperEndpointText}</FormHelperText>
-            </FormControl>
-          </Box>
+        {_.isEqual(test.sutRole, 'sender') && _.has(test, 'inputs') && test.inputs !== undefined && (
+          <StepText inputs={test.inputs} role={test.sutRole} />
         )}
-        {timeoutInput !== null && (
-          <Box sx={flexColumnStyle}>
-            <FormControl fullWidth>
-              <TextField fullWidth id="card-input" label={timeoutInput} variant="outlined" />
-              <FormHelperText>{helperTimeoutText}</FormHelperText>
-            </FormControl>
-          </Box>
-        )}
-        {outgoingInput !== null && (
-          <Box sx={flexColumnStyle}>
-            <FormControl fullWidth>
-              <TextField fullWidth id="card-input" label={outgoingInput} variant="outlined" />
-              <FormHelperText>{helperOutgoingText}</FormHelperText>
-            </FormControl>
-          </Box>
-        )}
-        {portInput !== null && (
-          <Box sx={flexColumnStyle}>
-            <FormControl fullWidth>
-              <TextField fullWidth id="card-input" label={portInput} variant="outlined" />
-              <FormHelperText>{helperText}</FormHelperText>
-            </FormControl>
-          </Box>
-        )}
-        {ipInput !== null && (
-          <Box sx={flexColumnStyle}>
-            <FormControl fullWidth>
-              <TextField fullWidth id="card-input" label={ipInput} variant="outlined" />
-              <FormHelperText>{helperTimeoutText}</FormHelperText>
-            </FormControl>
-          </Box>
-        )}
+        {_.has(test, 'inputs') &&
+          test.inputs !== undefined &&
+          test.inputs.map((input) => {
+            return (
+              <Box sx={flexColumnStyle} key={input.key}>
+                <FormControl fullWidth>
+                  <TextField fullWidth id="card-input" label={input.name} variant="outlined" />
+                  <FormHelperText>{input.hoverlabel}</FormHelperText>
+                </FormControl>
+              </Box>
+            )
+          })}
       </CardContent>
       <Divider />
 
+      {/* Note: This might change with functionality to generate the endpoint*/}
       <Box
         sx={{
           display: 'flex',
@@ -130,25 +137,33 @@ const TestCard = ({
           pr: 2,
         }}
       >
-        <Box width={'50%'}>
-          {links && links.length > 0 && (
+        {_.includes(
+          ['10', '11', '12', '32mu2', '33mu2', '34mu2', '35mu2', '36mu2', '37mu2', '38mu2', '43mu2', '44mu2'],
+          test.ID
+        ) && (
+          <Box width={'50%'}>
             <Box>
-              {links.map((link, index) => (
-                <Button
-                  sx={{ ml: 2 }}
-                  key={index}
-                  color="secondary"
-                  endIcon={<ContentPasteGoIcon color="secondary" />}
-                  onClick={() => handleClick(link.href)}
-                >
-                  {link.label}
-                </Button>
-              ))}
+              <Button
+                sx={{ ml: 2 }}
+                color="secondary"
+                endIcon={<ContentPasteGoIcon color="secondary" />}
+                onClick={() => handleClick(endpoint + 'edge-ttp__' + test.ID + '/rep/xdrpr')}
+              >
+                Endpoint
+              </Button>
+              <Button
+                sx={{ ml: 2 }}
+                color="secondary"
+                endIcon={<ContentPasteGoIcon color="secondary" />}
+                onClick={() => handleClick(endpointTLS + 'edge-ttp__' + test.ID + '/rep/xdrpr')}
+              >
+                Endpoint TLS
+              </Button>
             </Box>
-          )}
-        </Box>
+          </Box>
+        )}
 
-        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', gap: 1 }}>
+        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', gap: 1, pl: 2 }}>
           <Button variant="contained" color="primary">
             RUN
           </Button>
