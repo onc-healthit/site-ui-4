5a752d1e4f57cb74fcc04f37baa9254b05dc9bc1
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-03-25 14:44:52 -0400

    ui: add additional faq cards to faq home

--- src/components/resources/FAQHome.tsx
+++ src/components/resources/FAQHome.tsx
@@ -1,19 +1,27 @@
-import { Container } from '@mui/material'
+'use client'
+import { Card, CardContent, CardHeader, Container, Typography } from '@mui/material'
 import Box from '@mui/material/Box'
 import Overview from '@/components/resources/FAQCards/OverviewCard'
 import Direct from '@/components/resources/FAQCards/DirectCard'
+import CCDA from '@components/resources/FAQCards/CCDACard'
+import XDM from '@components/resources/FAQCards/XDMCard'
+import XDR from '@components/resources/FAQCards/XDRCard'
+import SMTP from '@components/resources/FAQCards/SMTPCard'
+import OTHER from '@components/resources/FAQCards/OtherCard'
+
 import BannerBox from '@shared/BannerBox'
 import SubMenu, { menuProps } from '@/components/shared/SubMenu'
 
 export default function FAQHome() {
   const menuItems: menuProps[] = [
     { heading: 'Overview', href: '#overview' },
-    { heading: 'C-CDA', href: '' },
+    { heading: 'C-CDA', href: '#ccda' },
     { heading: 'Direct', href: '#direct' },
-    { heading: 'Edge', href: '' },
-    { heading: 'XDM', href: '' },
-    { heading: 'Other', href: '' },
-    { heading: 'Contact Us', href: '' },
+    { heading: 'XDM', href: '#xdm' },
+    { heading: 'XDR', href: '#xdr' },
+    { heading: 'SMTP', href: '#smtp' },
+    { heading: 'Other', href: '#other' },
+    { heading: 'Contact Us', href: '#contact' },
   ]
 
   return (
@@ -34,8 +42,41 @@ export default function FAQHome() {
             <Box id="overview">
               <Overview />
             </Box>
+            <Box id="ccda">
+              <CCDA />
+            </Box>
             <Box id="direct">
               <Direct />
+            </Box>
+            <Box id="xdm">
+              <XDM />
+            </Box>
+            <Box id="xdr">
+              <XDR />
+            </Box>
+            <Box id="smtp">
+              <SMTP />
+            </Box>
+            <Box id="other">
+              <OTHER />
+            </Box>
+            <Box>
+              <Card>
+                <CardHeader titleTypographyProps={{ fontWeight: 700 }} title="Still need help?" />
+                <CardContent>
+                  <Typography gutterBottom>
+                    Contact us at <a href="mailto:edge-test-tool@googlegroups.com">edge-test-tool@googlegroups.com </a>
+                    or <a href="https://groups.google.com/g/edge-test-tool">access our forum</a> for the lastest
+                    questions & answers.
+                  </Typography>
+                  <Typography variant="body2">
+                    Questions about the applicability of the initial set of standards, implementation specifications,
+                    and certification criteria should be directed to ONC at{' '}
+                    <a href="mailto:ONC.Certification@hhs.gov">ONC.Certification@hhs.gov</a>. Questions about functions
+                    and activities of the ATCBs should be directed to ONC at ONC.Certification@hhs.gov.
+                  </Typography>
+                </CardContent>
+              </Card>
             </Box>
           </Box>
         </Box>
