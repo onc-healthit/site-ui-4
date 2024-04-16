a8af1cbef92ccc0265782c896fed38aca52c3089
mattystank <32516464+mattystank@users.noreply.github.com>
2024-04-16 11:54:24 -0400

    ui: change next link to html link and comment out DrawerHeader (#51)
    Co-authored-by: Matthew Stankiewicz <stankm@pointclickcare.com>

--- src/components/shared/nav/nav/NavHeader.tsx
+++ src/components/shared/nav/nav/NavHeader.tsx
@@ -1,20 +1,20 @@
+import { Box, Divider, IconButton } from '@mui/material'
+import { Theme } from '@mui/material/styles'
+
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
-import Divider from '@mui/material/Divider'
-import IconButton from '@mui/material/IconButton'
-import { Theme, styled } from '@mui/material/styles'
+
 import Image from 'next/image'
-import Link from 'next/link'
 import { NAV_THICKER_DIVIDER } from '@/constants/navConstants'
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}))
+// const DrawerHeader = styled('div')(({ theme }) => ({
+//   display: 'flex',
+//   alignItems: 'center',
+//   justifyContent: 'flex-end',
+//   padding: theme.spacing(0, 1),
+//   // necessary for content to be below app bar
+//   ...theme.mixins.toolbar,
+// }))
 
 interface SiteNavHeaderProps {
   handleDrawerClose: () => void
@@ -23,21 +23,28 @@ interface SiteNavHeaderProps {
 export default function NavHeader({ handleDrawerClose, theme }: SiteNavHeaderProps) {
   return (
     <>
-      <DrawerHeader sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-        <Link href={'/'} passHref>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingX: '8px',
+          paddingY: '8px',
+        }}
+      >
+        <a href="/">
           <Image
             src="/shared/site-nav-logo.svg"
             width={250}
             height={42}
             alt="SITE logo with text: The hub for testing tools & resources"
             priority
-            // style={{ width: 287, height: 'auto' }}
           />
-        </Link>
+        </a>
         <IconButton onClick={handleDrawerClose}>
           {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
         </IconButton>
-      </DrawerHeader>
+      </Box>
       <Divider sx={{ borderWidth: NAV_THICKER_DIVIDER }} />
     </>
   )
