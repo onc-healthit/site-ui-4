96122f51a4b6ca18d4ae9184668342abdabfaaf3
Dan Brown <dbrown.se@gmail.com>
2024-05-29 18:47:12 -0400

    Move ScorecardHome to correct location

--- src/app/c-cda/scorecard/page.tsx
+++ src/app/c-cda/scorecard/page.tsx
@@ -1,4 +1,4 @@
-import ScorecardHome from '@/components/c-cda/additional/ScorecardHome'
+import ScorecardHome from '@/components/c-cda/additional/scorecard/ScorecardHome'
 
 export default function Scorecard() {
   return <ScorecardHome />
