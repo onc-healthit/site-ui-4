f74e16e4b4228c492fc921b41e800c564350f661
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-06-03 12:39:54 -0400

    ui: add theme over error display and reformat error pop-up

--- src/components/c-cda/validation/results/ErrorDisplay.tsx
+++ src/components/c-cda/validation/results/ErrorDisplay.tsx
@@ -1,5 +1,11 @@
-import { Dialog, DialogTitle, IconButton, DialogContent, Typography } from '@mui/material'
+import React from 'react'
+import { Box, Dialog, DialogTitle, IconButton, DialogContent, Typography } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
+import { ErrorOutline } from '@mui/icons-material'
+import palette from '@/styles/palette'
+import ONCLogo from '@public/shared/ONCLogo-backgroundImage.png'
+import Image from 'next/image'
+
 interface ErrorDisplayCardProps {
   open: boolean
   handleClose: () => void
@@ -9,14 +15,45 @@ const ErrorDisplayCard = ({ open, handleClose, response }: ErrorDisplayCardProps
   return (
     <>
       {response ? (
-        <Dialog open={open} maxWidth="sm">
-          <DialogTitle typography={'h3'} sx={{ fontWeight: '600', pb: 0 }} id="validating-dialog-title">
-            {'Error Status '} {response.errorStatus!}
-          </DialogTitle>
+        <Dialog open={open} maxWidth="sm" onClose={handleClose}>
           <IconButton aria-label="Close Dialog" sx={{ position: 'absolute', right: 8, top: 8 }} onClick={handleClose}>
-            <CloseIcon />
+            <CloseIcon htmlColor={palette.white} />
           </IconButton>
-          <DialogContent>
+          <Box px={4} py={3} alignItems={'flex-start'} display={'flex'} flexDirection={'row'} bgcolor={palette.warning}>
+            <Image
+              style={{
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                top: -80,
+                left: -1,
+                position: 'absolute',
+                overflow: 'clip',
+                width: '100%',
+                height: '100%',
+                objectFit: 'none',
+              }}
+              src={ONCLogo}
+              alt="ONC Logo"
+            />
+            <ErrorOutline sx={{ mb: -1 }} fontSize={'large'} htmlColor={palette.white} />
+            <Box>
+              <DialogTitle
+                textAlign={'left'}
+                color={palette.white}
+                sx={{ fontWeight: '600', px: 2, py: 1 }}
+                id="validating-dialog-title"
+                variant="h3"
+              >
+                Oh no!
+                <br />
+                Looks like there was an error.
+              </DialogTitle>
+              <Typography sx={{ px: 2 }} textAlign={'left'} color={palette.white}>
+                Error Type: {response.errorStatus!}
+              </Typography>
+            </Box>
+          </Box>
+          <DialogContent sx={{ padding: '32px' }}>
             <Typography>{response.error}</Typography>
           </DialogContent>
         </Dialog>
