52f6e9df439274106ce3ccefbf85094d95c90399
Dan Brown <dbrown.se@gmail.com>
2024-06-05 22:12:26 -0400

    Create early static version of SC Heat Map

--- src/components/c-cda/additional/scorecard/next-steps/ScorecardNextSteps.tsx
+++ src/components/c-cda/additional/scorecard/next-steps/ScorecardNextSteps.tsx
@@ -5,15 +5,15 @@ import ScorecardDetailedResults from './ScorecardDetailedResults'
 export default function ScorecardNextSteps() {
   return (
     <Box>
-      <Box sx={{ pb: 2 }}>
+      <Box sx={{ pb: 3 }}>
         <Typography variant="h3" sx={{ fontWeight: 'bold', pt: 2, pb: 2 }}>
           Next Steps
         </Typography>
         <Typography variant="h6">
-          Grades and the number of issues for each of the sections of information present in your document are shown
-          below. You can use it to quickly identify areas within the document that require the most attention. Click on
-          each of the buttons to navigate to the appropriate part of the report that contains additional details on the
-          identified issues.
+          The number of issues and grade for each of the sections of information present in your document are shown in
+          the &quot;Heat Map&quot; below. You can use it to quickly identify areas within the document that require the
+          most attention. Click on each of the buttons with identified issues to navigate to the relevant detailed
+          results. Note: Sections with errors and sections which do not exist in the document do not receive a grade.
         </Typography>
       </Box>
       <ScorecardHeatMap></ScorecardHeatMap>
