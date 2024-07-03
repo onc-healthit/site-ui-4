3d3749c0d6beab7eaf86ccfada721248f526e2ad
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-07-03 13:59:52 -0400

    feat: create local install component and page, modified css & add back to top page

---
+++ src/app/local-install/page.tsx
@@ -0,0 +1,28 @@
+// Add 'use client' at the top if using Next.js 13
+'use client'
+
+import React, { useState, useEffect } from 'react'
+import MarkdownPage from '@/components/local-install'
+
+const LocalInstallMarkdown: React.FC = () => {
+  const [markdownContent, setMarkdownContent] = useState<string>('')
+
+  useEffect(() => {
+    fetch('/markdown/local-installation-dev.md')
+      .then((response) => response.text())
+      .then((data) => setMarkdownContent(data))
+      .catch((error) => console.error('Error fetching Markdown:', error))
+  }, [])
+
+  return <MarkdownPage content={markdownContent} />
+}
+
+const LocalInstallHome: React.FC = () => {
+  return (
+    <>
+      <LocalInstallMarkdown />
+    </>
+  )
+}
+
+export default LocalInstallHome
