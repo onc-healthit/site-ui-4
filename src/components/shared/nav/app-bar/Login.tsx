a0a689a9f99095ea15e0570bf0eb987705310259
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-08-12 15:35:18 -0400

    ui: add show/hide adornments and add a maxWidth

--- src/components/shared/nav/app-bar/Login.tsx
+++ src/components/shared/nav/app-bar/Login.tsx
@@ -1,8 +1,20 @@
 'use client'
+import palette from '@/styles/palette'
 import Close from '@mui/icons-material/Close'
-import { Grid, TextField, Card, CardHeader, CardContent, IconButton, Button, styled } from '@mui/material'
+import {
+  Grid,
+  InputAdornment,
+  TextField,
+  Card,
+  CardHeader,
+  CardContent,
+  IconButton,
+  Button,
+  styled,
+} from '@mui/material'
 import { signIn } from 'next-auth/react'
 import React, { useState } from 'react'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
 
 const StyledLoginButtonRowWrapper = styled('div')(() => ({
   paddingTop: 24,
@@ -19,23 +31,30 @@ const LoginButtonStyle = {
 const Login = (props: { handleAuthClose: () => void }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+
   const handleLogin = () => {
     signIn('credentials', { username: email, password: password })
   }
+
+  const handleClickShowPassword = () => {
+    setShowPassword((prev) => !prev)
+  }
+
   return (
     <>
-      <Card>
+      <Card elevation={10} sx={{ maxWidth: '350px' }}>
         <CardHeader
           title="Login"
           action={
             <IconButton aria-label="settings" onClick={() => props.handleAuthClose()}>
-              <Close />
+              <Close fontSize="small" />
             </IconButton>
           }
-          sx={{ typography: 'title', fontWeight: 'bold' }}
+          titleTypographyProps={{ fontWeight: '600', variant: 'h3', color: palette.primary }}
         />
         <CardContent>
-          <Grid container spacing={3}>
+          <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
                 label="Email (Or Username)"
@@ -49,19 +68,28 @@ const Login = (props: { handleAuthClose: () => void }) => {
               <TextField
                 label="Password"
                 placeholder="Enter password"
-                type="password"
+                type={showPassword ? 'text' : 'password'}
                 variant="outlined"
                 required
                 fullWidth
                 onChange={(e) => setPassword(e.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword} edge="end">
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
               />
-              <Grid item xs={12}>
-                <StyledLoginButtonRowWrapper>
-                  <Button style={LoginButtonStyle} variant="contained" size="medium" onClick={handleLogin}>
-                    SIGN IN
-                  </Button>
-                </StyledLoginButtonRowWrapper>
-              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <StyledLoginButtonRowWrapper>
+                <Button style={LoginButtonStyle} variant="contained" size="medium" onClick={handleLogin}>
+                  SIGN IN
+                </Button>
+              </StyledLoginButtonRowWrapper>
             </Grid>
           </Grid>
         </CardContent>
