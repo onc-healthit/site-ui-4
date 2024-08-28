60b293657434d61ab35ac34cfd635343e4a4d1e8
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-08-28 13:53:54 -0400

    ui: revert location of sort switch

--- src/components/c-cda/scorecard/next-steps/ScorecardNextSteps.tsx
+++ src/components/c-cda/scorecard/next-steps/ScorecardNextSteps.tsx
@@ -46,14 +46,6 @@ export default function ScorecardNextSteps({
           <Typography variant="h3" sx={{ fontWeight: 'bold', pt: 2, pb: 2 }}>
             Next Steps
           </Typography>
-        </Box>
-        <Typography variant="h6">
-          The number of issues and grade for each of the sections of information present in your document are shown in
-          the &quot;Heat Map&quot; below. You can use it to quickly identify areas within the document that require the
-          most attention. Click on each of the buttons with identified issues to navigate to the relevant detailed
-          results. Note: Sections with errors and sections which do not exist in the document do not receive a grade.
-        </Typography>
-        <Box display={'flex'} px={1} py={2}>
           <StyledFormControlLabelWithLabelOnLeft
             control={
               <Switch
@@ -67,8 +59,13 @@ export default function ScorecardNextSteps({
             name="wrapped"
           />
         </Box>
+        <Typography variant="h6">
+          The number of issues and grade for each of the sections of information present in your document are shown in
+          the &quot;Heat Map&quot; below. You can use it to quickly identify areas within the document that require the
+          most attention. Click on each of the buttons with identified issues to navigate to the relevant detailed
+          results. Note: Sections with errors and sections which do not exist in the document do not receive a grade.
+        </Typography>
       </Box>
-
       <ScorecardHeatMap results={results}></ScorecardHeatMap>
       <Box id={removeHashtagToUseHrefLinkAsIdForAnchor(HrefLinkValueEnum.DETAILED_RESULTS)}>
         <ScorecardDetailedResults
