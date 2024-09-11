9712ff71ee9d7492724d44fb895d6598aa90829a
ebrockainq <ebrock@ainq.com>
2024-09-11 12:51:35 -0400

    Language fix and null value handling

--- src/components/direct/test-by-criteria/ServerActions.ts
+++ src/components/direct/test-by-criteria/ServerActions.ts
@@ -145,7 +145,7 @@ export async function handleXDRAPICall(data: XDRAPICallData): Promise<XDRAPIResp
     const content = response.data
 
     const testRequest = content.content.value.request || content.message
-    const testResponse = content.content.value.response
+    const testResponse = content.content.value.response || ''
 
     return {
       criteriaMet: content.status,
