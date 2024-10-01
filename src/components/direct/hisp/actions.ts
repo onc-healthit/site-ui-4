2ff2dc17df5b5de9f2c41f588cf3035133559e6b
Brian Harris <sendbrianemail@gmail.com>
2024-10-01 09:42:52 -0400

    can delete a profile

--- src/components/direct/hisp/actions.ts
+++ src/components/direct/hisp/actions.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { authOptions } from '@/lib/auth'
+import { StringIterator } from 'lodash'
 import { getServerSession } from 'next-auth'
 
 const ETT_API_URL = process.env.ETT_API_URL
@@ -36,6 +37,35 @@ export async function saveProfile(data: Profile) {
         profileName: profilename,
         username: session?.user?.name,
       }),
+    })
+    const data = await response.json()
+    if (!response.ok) {
+      console.log(`Error: ${data}`)
+    }
+    return data
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return JSON.stringify({
+        status: 500,
+        success: false,
+        error: error.message || 'An error occurred',
+      })
+    }
+  }
+}
+
+export async function deleteProfile(profilename: string) {
+  console.log(`deleting profile: ${profilename}`)
+  const session = await getServerSession(authOptions)
+  const jsessionid = session?.user?.jsessionid ?? ''
+  const ettAPIUrl = `${ETT_API_URL}/smtpProfile/${profilename}`
+  try {
+    const response = await fetch(ettAPIUrl, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Cookie: `JSESSIONID=${jsessionid}`,
+      },
     })
     const data = await response.json()
     if (!response.ok) {
--- src/components/direct/shared/Profile.tsx
+++ src/components/direct/shared/Profile.tsx
@@ -9,14 +9,29 @@ import {
   Select,
   MenuItem,
   SelectChangeEvent,
+  LinearProgress,
 } from '@mui/material'
 import { useContext, useEffect, useState } from 'react'
 import { ProfileContext } from '../hisp/context'
-import { fetchProfiles, saveProfile } from '../hisp/actions'
+import { deleteProfile, fetchProfiles, saveProfile } from '../hisp/actions'
 import { useSession } from 'next-auth/react'
 import AlertSnackbar from './AlertSnackbar'
 import _ from 'lodash'
 
+interface Profile {
+  profileName: string
+  sutSMTPAddress: string
+  sutEmailAddress: string
+  sutUsername: string
+  sutPassword: string
+  useTLS: boolean
+  smtpEdgeProfileID: string
+}
+
+const NEWPROFILENAME = '__new__'
+const removeProfilesWithNullProfileName = (profiles: Profile[]) => {
+  return profiles.filter((profile) => profile.profileName !== null)
+}
 const Profile = () => {
   const {
     setProfileid,
@@ -35,30 +50,20 @@ const Profile = () => {
     username,
   } = useContext(ProfileContext)
   const { data: session, status } = useSession()
-  interface Profile {
-    profileName: string
-    sutSMTPAddress: string
-    sutEmailAddress: string
-    sutUsername: string
-    sutPassword: string
-    useTLS: boolean
-    smtpEdgeProfileID: string
-  }
 
   const [profiles, setProfiles] = useState<Profile[]>([])
-  const [selectedProfileIndex, setSelectedProfileIndex] = useState(0)
+  const [selectedProfileName, setSelectedProfileName] = useState(NEWPROFILENAME)
   const [isLoading, setIsLoading] = useState(false)
   const [message, setMessage] = useState('')
 
   useEffect(() => {
     async function fetchLoggedInUsersProfiles() {
+      setIsLoading(true)
       const loggedInUsersProfiles = await fetchProfiles()
-      const filteredProiles = loggedInUsersProfiles.filter(
-        (profile: { profileName: null }) => profile.profileName !== null
-      )
+      const filteredProiles = removeProfilesWithNullProfileName(loggedInUsersProfiles)
       if (!_.isEmpty(filteredProiles)) {
         setProfiles(filteredProiles)
-        setSelectedProfileIndex(0)
+        setSelectedProfileName(filteredProiles[0].profileName)
         setProfilename(filteredProiles[0].profileName)
         setHostname(filteredProiles[0].sutSMTPAddress)
         setEmail(filteredProiles[0].sutEmailAddress)
@@ -67,6 +72,7 @@ const Profile = () => {
         setTls(filteredProiles[0].useTLS)
         setProfileid(filteredProiles[0].smtpEdgeProfileID)
       }
+      setIsLoading(false)
       return filteredProiles
     }
     if (status === 'authenticated') {
@@ -85,28 +91,59 @@ const Profile = () => {
       istls: tls,
       profilename: profilename,
     }).then(async (response) => {
-      setIsLoading(false)
-      setMessage('Profile saved successfully.')
+      setMessage(`${profilename} saved`)
       const loggedInUsersProfiles = await fetchProfiles()
-      const filteredProiles = loggedInUsersProfiles.filter(
-        (profile: { profileName: null }) => profile.profileName !== null
-      )
+      const filteredProiles = removeProfilesWithNullProfileName(loggedInUsersProfiles)
       setProfiles(filteredProiles)
-      setSelectedProfileIndex(profiles.length - 1)
-      setProfilename(filteredProiles[filteredProiles.length - 1].profileName)
-      setHostname(filteredProiles[filteredProiles.length - 1].sutSMTPAddress)
-      setEmail(filteredProiles[filteredProiles.length - 1].sutEmailAddress)
-      setUsername(filteredProiles[filteredProiles.length - 1].sutUsername)
-      setPassword(filteredProiles[filteredProiles.length - 1].sutPassword)
-      setTls(filteredProiles[filteredProiles.length - 1].useTLS)
-      setProfileid(filteredProiles[filteredProiles.length - 1].smtpEdgeProfileID)
+      if (selectedProfileName === NEWPROFILENAME) {
+        const lastProfile = _.last(filteredProiles)
+        console.log(`DEBUG: lastProfile: ${JSON.stringify(lastProfile)}`)
+        if (lastProfile) {
+          setSelectedProfileName(lastProfile.profileName)
+          setProfilename(lastProfile.profileName)
+          setHostname(lastProfile.sutSMTPAddress)
+          setEmail(lastProfile.sutEmailAddress)
+          setUsername(lastProfile.sutUsername)
+          setPassword(lastProfile.sutPassword)
+          setTls(lastProfile.useTLS)
+          setProfileid(lastProfile.smtpEdgeProfileID)
+        }
+      }
+      setIsLoading(false)
+      //handleProfileChange({ target: { value: selectedProfile.smtpEdgeProfileID } } as SelectChangeEvent)
+    })
+  }
+
+  const handleDeleteProfile = () => {
+    setIsLoading(true)
+    const profileNameToDelete = selectedProfileName
+    deleteProfile(profileNameToDelete).then(async (response) => {
+      setMessage(`${profilename} deleted`)
+      const loggedInUsersProfiles = await fetchProfiles()
+      const filteredProfiles = removeProfilesWithNullProfileName(loggedInUsersProfiles)
+      setProfiles(filteredProfiles || [{ smtpEdgeProfileID: NEWPROFILENAME } as Profile])
+      const lastProfile = _.last(filteredProfiles)
+      console.log(`DEBUG: lastProfile: ${JSON.stringify(lastProfile)}`)
+
+      setSelectedProfileName(lastProfile?.smtpEdgeProfileID || NEWPROFILENAME)
+      setProfilename(lastProfile?.profileName || '')
+      setHostname(lastProfile?.sutSMTPAddress || '')
+      setEmail(lastProfile?.sutEmailAddress || '')
+      setUsername(lastProfile?.sutUsername || '')
+      setPassword(lastProfile?.sutPassword || '')
+      setTls(lastProfile?.useTLS || false)
+      setProfileid(lastProfile?.smtpEdgeProfileID || NEWPROFILENAME)
+
+      setIsLoading(false)
+      //handleProfileChange({ target: { value: selectedProfile.smtpEdgeProfileID } } as SelectChangeEvent)
     })
   }
 
   const handleProfileChange = async (event: SelectChangeEvent) => {
-    const selectedProfileIndex = _.toNumber(event.target.value)
-    if (selectedProfileIndex === -1) {
-      setSelectedProfileIndex(selectedProfileIndex)
+    const selectedProfileName = event.target.value
+    if (selectedProfileName === NEWPROFILENAME) {
+      console.log('New Profile')
+      setSelectedProfileName(selectedProfileName)
       setProfilename('')
       setHostname('')
       setEmail('')
@@ -116,114 +153,120 @@ const Profile = () => {
       setProfileid('')
       return
     }
-    setSelectedProfileIndex(selectedProfileIndex)
-    setProfilename(profiles[selectedProfileIndex].profileName)
-    setHostname(profiles[selectedProfileIndex].sutSMTPAddress)
-    setEmail(profiles[selectedProfileIndex].sutEmailAddress)
-    setUsername(profiles[selectedProfileIndex].sutUsername)
-    setPassword(profiles[selectedProfileIndex].sutPassword)
-    setTls(profiles[selectedProfileIndex].useTLS)
-    setProfileid(profiles[selectedProfileIndex].smtpEdgeProfileID)
+    const selectedProfile = profiles.find((profile) => profile.profileName === selectedProfileName)
+    if (selectedProfile) {
+      setSelectedProfileName(selectedProfile.profileName)
+      setProfilename(selectedProfile.profileName)
+      setHostname(selectedProfile.sutSMTPAddress)
+      setEmail(selectedProfile.sutEmailAddress)
+      setUsername(selectedProfile.sutUsername)
+      setPassword(selectedProfile.sutPassword)
+      setTls(selectedProfile.useTLS)
+      setProfileid(selectedProfile.smtpEdgeProfileID)
+    }
   }
 
   return (
-    <Box component="form" sx={{ backgroundColor: palette.white }}>
-      {profiles.length > 0 && (
-        <Select
-          fullWidth
-          title="Select a profile."
-          onChange={handleProfileChange}
-          value={_.toString(selectedProfileIndex)}
-        >
-          {profiles.map((profile, index) => {
-            return (
-              <MenuItem key={index} value={index}>
-                {profile.profileName}
+    <>
+      {isLoading ? (
+        <LinearProgress />
+      ) : (
+        <Box component="form" sx={{ backgroundColor: palette.white }}>
+          {!_.isEmpty(profiles) && (
+            <Select fullWidth title="Select a profile." onChange={handleProfileChange} value={selectedProfileName}>
+              {profiles.map((profile, index) => {
+                return (
+                  <MenuItem key={index} value={profile.profileName}>
+                    {profile.profileName}
+                  </MenuItem>
+                )
+              })}
+              <MenuItem key={-1} value={NEWPROFILENAME}>
+                New Profile
               </MenuItem>
-            )
-          })}
-          <MenuItem key={-1} value={-1}>
-            New Profile
-          </MenuItem>
-        </Select>
-      )}
-      <Box display={'flex'} flexDirection={'column'} gap={4} p={2}>
-        <Box display={'flex'} flexDirection={'row'} justifyContent={'space-between'} pt={2} gap={2}>
-          <TextField
-            fullWidth
-            id="outlined-vendor-hostname"
-            label="Vendor Hostname/IP"
-            helperText="Hostname/IP of the vendor SMTP sysem"
-            value={hostname || ''}
-            required
-            onChange={(e) => setHostname(e.target.value)}
-          />
-          <TextField
-            fullWidth
-            id="vendor-email"
-            label="Vendor Direct Email Address"
-            helperText="Email of the vendor SMTP system"
-            value={email || ''}
-            required
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Box>
-        <Box display={'flex'} flexDirection={'row'} justifyContent={'space-between'} gap={2}>
-          <TextField
-            fullWidth
-            id="vendor-username"
-            label="Vendor Username"
-            helperText="Username for the vendor SMTP system"
-            value={username || ''}
-            required
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <TextField
-            fullWidth
-            id="vendor-password"
-            label="Vendor Password"
-            helperText="Password for the vendor SMTP system"
-            type="password"
-            value={password || ''}
-            required
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Box>
-        <FormGroup sx={{ width: '50%' }}>
-          <FormControlLabel
-            control={<Switch color="secondary" defaultChecked value={tls} onChange={(e) => setTls(e.target.checked)} />}
-            label="TLS REQUIRED"
-            name="tlsRequired"
-          />
-        </FormGroup>
-      </Box>
-
-      <Box display={'flex'} flexDirection={'column'} justifyContent={'space-between'} p={2}>
-        {status === 'authenticated' && (
-          <>
-            <TextField
-              fullWidth
-              id="select-profile"
-              label="Profile Name"
-              value={profilename}
-              required
-              onChange={(e) => setProfilename(e.target.value)}
-            />
-            <Box display={'flex'} justifyContent="space-between" component="span" sx={{ pt: 3 }}>
-              <Button variant="outlined" sx={{ color: palette.primary }} onClick={() => handleSaveProfile()}>
-                Save
-              </Button>
-              <Button variant="text" sx={{ color: palette.errorDark }}>
-                Remove
-              </Button>
+            </Select>
+          )}
+          <Box display={'flex'} flexDirection={'column'} gap={4} p={2}>
+            <Box display={'flex'} flexDirection={'row'} justifyContent={'space-between'} pt={2} gap={2}>
+              <TextField
+                fullWidth
+                id="outlined-vendor-hostname"
+                label="Vendor Hostname/IP"
+                helperText="Hostname/IP of the vendor SMTP sysem"
+                value={hostname || ''}
+                required
+                onChange={(e) => setHostname(e.target.value)}
+              />
+              <TextField
+                fullWidth
+                id="vendor-email"
+                label="Vendor Direct Email Address"
+                helperText="Email of the vendor SMTP system"
+                value={email || ''}
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </Box>
-          </>
-        )}
-      </Box>
-      {!_.isEmpty(message) && (
-        <AlertSnackbar message={message} severity="success" open={true} onClose={() => setMessage('')} />
+            <Box display={'flex'} flexDirection={'row'} justifyContent={'space-between'} gap={2}>
+              <TextField
+                fullWidth
+                id="vendor-username"
+                label="Vendor Username"
+                helperText="Username for the vendor SMTP system"
+                value={username || ''}
+                required
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <TextField
+                fullWidth
+                id="vendor-password"
+                label="Vendor Password"
+                helperText="Password for the vendor SMTP system"
+                type="password"
+                value={password || ''}
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Box>
+            <FormGroup sx={{ width: '50%' }}>
+              <FormControlLabel
+                control={
+                  <Switch color="secondary" defaultChecked value={tls} onChange={(e) => setTls(e.target.checked)} />
+                }
+                label="TLS REQUIRED"
+                name="tlsRequired"
+              />
+            </FormGroup>
+          </Box>
+
+          <Box display={'flex'} flexDirection={'column'} justifyContent={'space-between'} p={2}>
+            {status === 'authenticated' && (
+              <>
+                <TextField
+                  fullWidth
+                  id="select-profile"
+                  label="Profile Name"
+                  value={profilename}
+                  required
+                  onChange={(e) => setProfilename(e.target.value)}
+                />
+                <Box display={'flex'} justifyContent="space-between" component="span" sx={{ pt: 3 }}>
+                  <Button variant="outlined" sx={{ color: palette.primary }} onClick={() => handleSaveProfile()}>
+                    Save
+                  </Button>
+                  <Button variant="text" sx={{ color: palette.errorDark }} onClick={() => handleDeleteProfile()}>
+                    Remove
+                  </Button>
+                </Box>
+              </>
+            )}
+          </Box>
+          {!_.isEmpty(message) && (
+            <AlertSnackbar message={message} severity="success" open={true} onClose={() => setMessage('')} />
+          )}
+        </Box>
       )}
-    </Box>
+    </>
   )
 }
 export default Profile
