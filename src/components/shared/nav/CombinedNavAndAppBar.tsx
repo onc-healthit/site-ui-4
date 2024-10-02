19c06a30a576ddb900b6f0b0dae9f6541aca93c4
Dan Brown <dbrown.se@gmail.com>
2024-10-02 18:38:06 -0400

    Allow debug mode true to hide nav by default

--- src/components/shared/nav/CombinedNavAndAppBar.tsx
+++ src/components/shared/nav/CombinedNavAndAppBar.tsx
@@ -8,8 +8,8 @@ import SiteAppBar from '@/components/shared/nav/app-bar/SiteAppBar'
 import Nav from '@/components/shared/nav/nav/Nav'
 
 export default function CombinedNavAndAppBar() {
-  // TODO: default to false based on DEV_MODE env var
-  const [open, setOpen] = React.useState(true)
+  // TODO: default to false based on NEXT_PUBLIC_IS_DEBUG_MODE env var
+  const [open, setOpen] = React.useState(!(process.env.NEXT_PUBLIC_IS_DEBUG_MODE === 'true'))
   const handleDrawerOpen = () => {
     setOpen(true)
   }
