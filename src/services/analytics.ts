07c4a1923cf788d194196989899c64512d17a941
Dan Brown <dbrown.se@gmail.com>
2024-10-08 13:32:30 -0400

    Update analytics to not depend on debug mode

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -1,3 +1,5 @@
+'use client'
+// Note: Explicitly defining 'use client' as this funciton uses 'window', which is part of the browser
 /*
 eventType: Describes the type of action being tracked, Button Click Form Submission, Dropdown Selection,
 Link Click, Sub Menu Anchor Link Click, etc.
@@ -5,15 +7,14 @@ eventCategory: Groups events together, such as by a specific tool name or naviga
 eventLabel: Provides specific details for the event such as what the action and outcome is after the event is fired
 */
 const eventTrack = (eventType: string, eventCategory: string, eventLabel: string) => {
-  if (typeof window.gtag === 'function') {
-    if (process.env.NEXT_PUBLIC_IS_DEBUG_MODE === 'true') {
+  if (process.env.NEXT_PUBLIC_IS_EVENT_TRACKING === 'true') {
+    if (typeof window.gtag === 'function') {
       console.info({
         event: eventType,
         eventCategory: eventCategory,
         eventLabel: eventLabel,
       })
-    }
-    if (process.env.NEXT_PUBLIC_IS_EVENT_TRACKING === 'true') {
+
       window.gtag('event', eventType, {
         event_category: eventCategory,
         event_label: eventLabel,
