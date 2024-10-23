c64f36d08200ab0a31c3d429e9d4541a27bde78c
ebrockainq <ebrock@ainq.com>
2024-10-23 14:15:49 -0400

    Minor fixes to fixed endpoints and H2 subgroups

--- src/components/direct/hisp/XDRTestCard.tsx
+++ src/components/direct/hisp/XDRTestCard.tsx
@@ -130,7 +130,7 @@ interface SelectedDocument {
 
 const TestCard = ({ test }: TestCardProps) => {
   const defaultEndpoint =
-    process.env.XDR_ENDPOINT_PREFIX || 'http://ett.healthit.gov:11084/xdstools/sim/edge-ttp__' + test.id + '/rep/xdrpr'
+    process.env.XDR_ENDPOINT_PREFIX || 'http://ett.healthit.gov:11080/xdstools/sim/edge-ttp__' + test.id + '/rep/xdrpr'
   const defaultEndpointTLS =
     process.env.XDR_ENDPOINT_TLS_PREFIX ||
     'https://ett.healthit.gov:11084/xdstools/sim/edge-ttp__' + test.id + '/rep/xdrpr'
