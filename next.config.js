b972961b1bceb2399b29d0763393935b2607cb68
Dan Brown <dbrown.se@gmail.com>
2024-10-22 21:51:29 -0400

    Redirect SITE 3 home and scorecard routes (#180)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        // Redirect old SITE 3 home route
+        source: '/home', // Match old SITE 3 home route
+        destination: '/', // Redirect to new SITE 4 home route
+        permanent: true, // Mark as 301 permanent redirect to update SEO and bookmarks
+      },
+      {
+        // Redirect old SITE 3 scorecard route, previously done with an Apache2 proxy for the C-CDA server
+        source: '/scorecard',
+        destination: '/c-cda/scorecard',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
