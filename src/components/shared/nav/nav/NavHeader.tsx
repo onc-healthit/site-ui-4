04c9d85f747693214269358391aa267516ebcf61
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-10-24 15:49:57 -0400

    ui: make nav adjust to browser width; 900px it defaults to closed

--- src/components/shared/nav/nav/NavHeader.tsx
+++ src/components/shared/nav/nav/NavHeader.tsx
@@ -1,27 +1,47 @@
+import { useEffect, useState } from 'react'
 import { Box, Divider, IconButton, Tooltip } from '@mui/material'
 import { Theme } from '@mui/material/styles'
-
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
-
 import Image from 'next/image'
 import SITELogo from '@public/shared/site-nav-logo.svg'
 import { NAV_THICKER_DIVIDER } from '@/constants/navConstants'
-
-// const DrawerHeader = styled('div')(({ theme }) => ({
-//   display: 'flex',
-//   alignItems: 'center',
-//   justifyContent: 'flex-end',
-//   padding: theme.spacing(0, 1),
-//   // necessary for content to be below app bar
-//   ...theme.mixins.toolbar,
-// }))
 
 interface SiteNavHeaderProps {
   handleDrawerClose: () => void
   theme: Theme
 }
+
 export default function NavHeader({ handleDrawerClose, theme }: SiteNavHeaderProps) {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen((prev) => {
+      if (prev) {
+        handleDrawerClose() // Close drawer if it was open
+      }
+      return !prev // Toggle the state
+    })
+  }
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 900 && isDrawerOpen) {
+        handleDrawerClose()
+        setIsDrawerOpen(false) // Update local state to closed
+      } else if (window.innerWidth >= 900 && !isDrawerOpen) {
+        setIsDrawerOpen(true) // Optionally open if above 900px
+      }
+    }
+
+    window.addEventListener('resize', handleResize) // Attach resize listener
+    handleResize() // Initial check on render
+
+    return () => {
+      window.removeEventListener('resize', handleResize) // Clean up listener on unmount
+    }
+  }, [handleDrawerClose, isDrawerOpen]) // Include isDrawerOpen in the dependency array
+
   return (
     <>
       <Box
@@ -41,8 +61,8 @@ export default function NavHeader({ handleDrawerClose, theme }: SiteNavHeaderPro
             priority
           />
         </a>
-        <Tooltip arrow placement="right" title="Close Navigation">
-          <IconButton size="small" aria-label="Toggle Navigation" onClick={handleDrawerClose}>
+        <Tooltip arrow placement="right" title="Toggle Navigation">
+          <IconButton size="small" aria-label="Toggle Navigation" onClick={toggleDrawer}>
             {theme.direction === 'rtl' ? (
               <ChevronRightIcon aria-label="Open Navigation" />
             ) : (
