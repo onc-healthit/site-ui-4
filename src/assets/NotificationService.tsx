f587da0cab3110e948a0f767c032805ba6147060
mattystank <32516464+mattystank@users.noreply.github.com>
2024-10-30 13:26:42 -0400

    Delete scheduled maintenance notification (#198)

--- src/assets/NotificationService.tsx
+++ src/assets/NotificationService.tsx
@@ -9,12 +9,6 @@ export const fetchNotifications = async (): Promise<Announcement[]> => {
   return [
     {
       id: '1',
-      title: 'Scheduled Maintenance',
-      content:
-        'Our site will be undergoing scheduled maintenance on Friday, July 28th, from 2 AM to 4 AM (UTC). During this time, some services may be unavailable. We apologize for any inconvenience and appreciate your patience.',
-    },
-    {
-      id: '2',
       title: 'Monthly Updates',
       content:
         'We regularly implement monthly updates to enhance your experience with our SITE tools. To stay informed about the latest features, improvements, and any important changes, we encourage you to review our release notes. You can find them in the bottom left corner of the navigation. Keeping up with these updates will help you maximize the benefits of our tools and ensure you’re using them to their full potential!',
