1e69feaeb8af6619188a81237648c93ec4f7287e
Dan Brown <dbrown.se@gmail.com>
2024-11-08 13:03:59 -0500

    Update release date logic/view in FABs and history

--- src/assets/ReleaseService.ts
+++ src/assets/ReleaseService.ts
@@ -1,20 +1,35 @@
 export async function fetchReleaseData() {
-  const releaseVersionURL = process.env.NEXT_PUBLIC_RELEASE_VERSION_URL
-  const releaseDateURL = process.env.NEXT_PUBLIC_RELEASE_DATE_URL
+  const releaseVersionText: string = await fetchReleaseVersion()
+  const releaseDateText: string = await fetchReleaseDate()
+  return { releaseVersionText, releaseDateText }
+}
 
-  if (!releaseVersionURL || !releaseDateURL) {
-    throw new Error('Release URLs are not defined in the environment variables')
+export async function fetchReleaseVersion() {
+  const releaseVersionURL = process.env.NEXT_PUBLIC_RELEASE_VERSION_URL
+  if (!releaseVersionURL) {
+    throw new Error('Release Version URL is not defined in the environment variables')
   }
 
   const responseVersion = await fetch(releaseVersionURL)
-  const responseDate = await fetch(releaseDateURL)
-
-  if (!responseVersion.ok || !responseDate.ok) {
-    throw new Error('Failed to fetch release data')
+  if (!responseVersion.ok) {
+    throw new Error('Failed to fetch release Version')
   }
 
-  const releaseVersionHTML = await responseVersion.text()
-  const releaseDateHTML = await responseDate.text()
+  const releaseVersionText = await responseVersion.text()
+  return releaseVersionText
+}
 
-  return { releaseVersionHTML, releaseDateHTML }
+export async function fetchReleaseDate() {
+  const releaseDateURL = process.env.NEXT_PUBLIC_RELEASE_DATE_URL
+  if (!releaseDateURL) {
+    throw new Error('Release Date URL is not defined in the environment variables')
+  }
+
+  const responseDate = await fetch(releaseDateURL)
+  if (!responseDate.ok) {
+    throw new Error('Failed to fetch release date')
+  }
+
+  const releaseDateText = await responseDate.text()
+  return releaseDateText
 }
