3cade924d2afba482129a11fc6c91a5cfa3c7ebe
Girish <bgirishrao@drajer.com>
2026-04-14 13:52:42 -0400

    update start only image

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -26,7 +26,7 @@ export default function Footer() {
               <Image style={{ width: '20px', height: 'auto' }} src={websiteLaunch} alt="Placeholder" />
             </Link>
             <Typography sx={{ mr: 6 }} color="white">
-              Office of the National Coordinator for Health Information Technology
+              Owned by Office of the National Coordinator for Health Information Technology
             </Typography>
           </Box>
         </Box>
